ded0d23beb99867e04ce82c4e8e65fed272d9c37
rishabhchulet <37954840+rishabhchulet@users.noreply.github.com>
2025-07-10 05:00:50 +0530

    Fix Metro Bundler Error

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,7 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
+      // Required for react-native-reanimated to work properly
       // NOTE: 'react-native-reanimated/plugin' has to be listed last.
       'react-native-reanimated/plugin',
     ],
